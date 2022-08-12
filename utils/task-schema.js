5605a3c7cd241874a76ea246f07b27b41c076c08
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-08-12 11:35:45 -0700

    updated to v17 Joi validation documentation

--- utils/task-schema.js
+++ utils/task-schema.js
@@ -1,8 +1,22 @@
-const Joi = require("joi");
+/*
+*** Older Joi format below ***
+
+const Joi = require('joi');
 
 const taskSchema = {
-  name: Joi.string().min(3).required(),
-  completed: Joi.boolean(),
+    name: Joi.string().min(3).required(),
+    completed: Joi.boolean()
 };
 
 exports.validateTask = (task) => Joi.validate(task, taskSchema);
+
+*/
+
+const Joi = require("joi");
+
+const taskSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  completed: Joi.boolean(),
+});
+
+exports.validateTask = (task) => taskSchema.validate(task);
